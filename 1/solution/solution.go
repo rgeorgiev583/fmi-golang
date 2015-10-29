7490c18966d57bac43c0db5631dc17af215cda88
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-10-29 11:31:16 +0200

    added source file with the final version of the solution to the first homework assignment

---
+++ 1/solution/solution.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"strings"
+)
+
+func ExtractColumn(logContents string, column uint8) string {
+	var buffer bytes.Buffer
+	scanner := bufio.NewScanner(strings.NewReader(logContents))
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line == "" {
+			continue
+		}
+
+		cols := strings.Split(line, " ")
+
+		switch column {
+		case 0:
+			buffer.WriteString(cols[0])
+			buffer.WriteString(" ")
+			buffer.WriteString(cols[1])
+		case 1:
+			buffer.WriteString(cols[2])
+		case 2:
+			buffer.WriteString(cols[3])
+
+			for j := 4; j < len(cols); j++ {
+				buffer.WriteString(" ")
+				buffer.WriteString(cols[j])
+			}
+		}
+
+		buffer.WriteString("\n")
+	}
+
+	return buffer.String()
+}
