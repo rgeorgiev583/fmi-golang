0edac4f1fcd961dda3b68a0afdb28c239e55f8d1
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-12-06 16:49:50 +0200

    fix: dropped implementations of marshalling and unmarshalling methods to/from JSON/XML on Book struct: unnecessary; struct field tags cannot be comma-separated: only by space; dropped redundant struct wrapper for Rating field: replaced with `>' ordering of (container, member) elements in field tag; optimize: dropped Person struct type: Author is now of anonymous struct type; dropped *copyCount fields from Book struct: unused

--- 3/Book.go
+++ 3/Book.go
@@ -2,47 +2,20 @@ package main
 
 import (
     "fmt"
-    "encoding/json"
     "encoding/xml"
 )
 
-type Person struct {
-    FirstName string `json:"first_name", xml:"first_name"`
-    LastName string `json:"last_name", xml:"last_name"`
-}
-
 type Book struct {
     XMLName xml.Name `xml:"book"`
-    ISBN string `json:"isbn", xml:"isbn,attr"`
-    Title string `json:"title", xml:"title"`
-    Author Person `json:"author", xml:"author"`
-    Ratings []struct {
-        Rating uint8 `xml:"rating"`
-    } `json:"ratings", xml:"ratings"`
-    availableCopyCount int
-    registeredCopyCount int
-}
-
-func (p *Person) Person() string {
-    return fmt.Sprintf("%v %v", p.FirstName, p.LastName)
+    ISBN string `json:"isbn" xml:"isbn,attr"`
+    Title string `json:"title" xml:"title"`
+    Author struct {
+        FirstName string `json:"first_name" xml:"first_name"`
+        LastName string `json:"last_name" xml:"last_name"`
+    } `json:"author" xml:"author"`
+    Ratings []uint8 `json:"ratings" xml:"ratings>rating"`
 }
 
 func (b *Book) String() string {
-    return fmt.Sprintf("[%v] %v от %v", b.ISBN, b.Title, b.Author)
-}
-
-func (b *Book) MarshalJSON() ([]byte, error) {
-    return json.Marshal(b)
-}
-
-func (b *Book) UnmarshalJSON(data []byte) error {
-    return json.Unmarshal(data, b)
-}
-
-func (b *Book) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
-    return e.EncodeElement(b, start)
-}
-
-func (b *Book) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-    return d.DecodeElement(b, &start)
+    return fmt.Sprintf("[%v] %v от %v %v", b.ISBN, b.Title, b.Author.FirstName, b.Author.LastName)
 }
