cf44bf17c2c34e876c3c0649ce1c835117c69879
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-12-29 18:02:50 +0200

    added generic ring buffer implementation

---
+++ 4/ringbuffer.go
@@ -0,0 +1,30 @@
+type RingBuffer struct {
+    buffer []interface{}
+    beginPos, endPos int
+}
+
+func (rb *RingBuffer) GetDataSlice() []interface{} {
+    if rb.beginPos <= rb.endPos {
+        return rb.buffer[rb.beginPos:rb.endPos+1]
+    } else {
+        return append(buffer[rb.beginPos:], rb.buffer[:rb.endPos+1]
+    }
+}
+
+func (rb *RingBuffer) Append(item interface{}) {
+    if rb.beginPos == rb.endPos + 1 {
+        rb.beginPos++
+    }
+
+    if rb.endPos == len(rb.buffer) - 1 {
+        rb.endPos = 0
+    } else {
+        rb.endPos++
+    }
+
+    if rb.beginPos == len(rb.buffer) - 1 {
+        rb.beginPos = 0
+    }
+
+    rb.buffer[rb.endPos] = item
+}
