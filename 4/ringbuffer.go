2d77e90a042800c743971c2b0c437c79abe50fe5
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-12-31 14:56:00 +0200

    go fmt everything

--- 4/ringbuffer.go
+++ 4/ringbuffer.go
@@ -1,86 +1,86 @@
 package main
 
 import (
-    "bytes"
-    "fmt"
+	"bytes"
+	"fmt"
 )
 
 type RingBuffer struct {
-    buffer []interface{}
-    beginPos, endPos int
+	buffer           []interface{}
+	beginPos, endPos int
 }
 
 type InvalidIndexError struct {
-    index int
+	index int
 }
 
 func (iie *InvalidIndexError) Error() string {
-    return fmt.Sprintf("%d is not a valid index for an element in this buffer", iie.index)
+	return fmt.Sprintf("%d is not a valid index for an element in this buffer", iie.index)
 }
 
 func (rb *RingBuffer) Length() int {
-    if rb.beginPos < rb.endPos {
-        return rb.endPos - rb.beginPos + 1
-    } else {
-        return len(rb.buffer)
-    }
+	if rb.beginPos < rb.endPos {
+		return rb.endPos - rb.beginPos + 1
+	} else {
+		return len(rb.buffer)
+	}
 }
 
 func (rb *RingBuffer) Item(index int) (interface{}, error) {
-    if index < 0 || index >= len(rb.buffer) || rb.beginPos == 0 && index > rb.endPos {
-        return nil, &InvalidIndexError{index}
-    }
+	if index < 0 || index >= len(rb.buffer) || rb.beginPos == 0 && index > rb.endPos {
+		return nil, &InvalidIndexError{index}
+	}
 
-    pos := rb.endPos - index
+	pos := rb.endPos - index
 
-    if pos < 0 {
-        pos += len(rb.buffer)
-    }
+	if pos < 0 {
+		pos += len(rb.buffer)
+	}
 
-    return rb.buffer[pos], nil
+	return rb.buffer[pos], nil
 }
 
 func (rb *RingBuffer) Append(value interface{}) {
-    if rb.beginPos == -1 || rb.endPos == -1 {
-        rb.beginPos = 0
-        rb.endPos = 0
-        rb.buffer[0] = value
-        return
-    }
+	if rb.beginPos == -1 || rb.endPos == -1 {
+		rb.beginPos = 0
+		rb.endPos = 0
+		rb.buffer[0] = value
+		return
+	}
 
-    if rb.endPos == len(rb.buffer) - 1 {
-        rb.endPos = 0
-    } else {
-        rb.endPos++
-    }
+	if rb.endPos == len(rb.buffer)-1 {
+		rb.endPos = 0
+	} else {
+		rb.endPos++
+	}
 
-    if rb.beginPos == len(rb.buffer) - 1 {
-        rb.beginPos = 0
-    } else if rb.beginPos == rb.endPos {
-        rb.beginPos++
-    }
+	if rb.beginPos == len(rb.buffer)-1 {
+		rb.beginPos = 0
+	} else if rb.beginPos == rb.endPos {
+		rb.beginPos++
+	}
 
-    rb.buffer[rb.endPos] = value
+	rb.buffer[rb.endPos] = value
 }
 
 func (rb *RingBuffer) String() string {
-    var buf bytes.Buffer
-    buf.WriteString("[ ")
+	var buf bytes.Buffer
+	buf.WriteString("[ ")
 
-    for i := 0; i < len(rb.buffer) - 1; i++ {
-        rbi, _ := rb.Item(i)
-        buf.WriteString(fmt.Sprintf("%v ", rbi))
-    }
+	for i := 0; i < len(rb.buffer)-1; i++ {
+		rbi, _ := rb.Item(i)
+		buf.WriteString(fmt.Sprintf("%v ", rbi))
+	}
 
-    rbi, _ := rb.Item(len(rb.buffer) - 1)
-    buf.WriteString(fmt.Sprintf("%v]", rbi))
-    return buf.String()
+	rbi, _ := rb.Item(len(rb.buffer) - 1)
+	buf.WriteString(fmt.Sprintf("%v]", rbi))
+	return buf.String()
 }
 
 func NewRingBuffer(size int) *RingBuffer {
-    return &RingBuffer{
-        buffer: make([]interface{}, size),
-        beginPos: -1,
-        endPos: -1,
-    }
+	return &RingBuffer{
+		buffer:   make([]interface{}, size),
+		beginPos: -1,
+		endPos:   -1,
+	}
 }
--- 4/ringbuffer_test.go
+++ 4/ringbuffer_test.go
@@ -1,72 +1,72 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestNewRingBuffer(t *testing.T) {
-    rb := NewRingBuffer(10)
+	rb := NewRingBuffer(10)
 
-    if rb == nil {
-        t.Errorf("could not create RingBuffer struct")
-    }
+	if rb == nil {
+		t.Errorf("could not create RingBuffer struct")
+	}
 }
 
 func appendNumbers(rb *RingBuffer, s []interface{}, count int, val *int, pos *int) {
-    for i := 0; i < count; i++ {
-        if *pos == -1 {
-            for j := len(s) - 1; j > 0; j-- {
-                s[j] = s[j - 1]
-            }
+	for i := 0; i < count; i++ {
+		if *pos == -1 {
+			for j := len(s) - 1; j > 0; j-- {
+				s[j] = s[j-1]
+			}
 
-            *pos++
-        }
+			*pos++
+		}
 
-        rb.Append(*val)
-        s[*pos] = *val
-        *val++
-        *pos--
-    }
+		rb.Append(*val)
+		s[*pos] = *val
+		*val++
+		*pos--
+	}
 }
 
 func logRingBuffer(t *testing.T, rb *RingBuffer, s []interface{}, message string) {
-    t.Log(message)
-    t.Logf("ring buffer: %v", rb)
-    t.Logf("slice: %v", s)
-    t.Logf("items in ring buffer: %v", rb)
-    t.Logf("items in slice: %v", s)
+	t.Log(message)
+	t.Logf("ring buffer: %v", rb)
+	t.Logf("slice: %v", s)
+	t.Logf("items in ring buffer: %v", rb)
+	t.Logf("items in slice: %v", s)
 }
 
 func testEqualityRingBufferSlice(t *testing.T, rb *RingBuffer, s []interface{}) {
-    if len(s) != len(rb.buffer) {
-        t.Errorf("ring buffer %v and slice %v should have had the same length", rb, s)
-    }
+	if len(s) != len(rb.buffer) {
+		t.Errorf("ring buffer %v and slice %v should have had the same length", rb, s)
+	}
 
-    for i := range s {
-        rbi, _ := rb.Item(i)
+	for i := range s {
+		rbi, _ := rb.Item(i)
 
-        if s[i] != rbi {
-            t.Errorf("ring buffer %v and slice %v should have had the same data", rb, s)
-        }
-    }
+		if s[i] != rbi {
+			t.Errorf("ring buffer %v and slice %v should have had the same data", rb, s)
+		}
+	}
 }
 
 func TestRingBufferAppend(t *testing.T) {
-    rb := NewRingBuffer(10)
-    s := make([]interface{}, 10)
+	rb := NewRingBuffer(10)
+	s := make([]interface{}, 10)
 
-    val := 1
-    pos := len(s) - 1
+	val := 1
+	pos := len(s) - 1
 
-    appendNumbers(rb, s, 10, &val, &pos)
-    logRingBuffer(t, rb, s, "after adding 1..10")
-    testEqualityRingBufferSlice(t, rb, s)
+	appendNumbers(rb, s, 10, &val, &pos)
+	logRingBuffer(t, rb, s, "after adding 1..10")
+	testEqualityRingBufferSlice(t, rb, s)
 
-    appendNumbers(rb, s, 5, &val, &pos)
-    logRingBuffer(t, rb, s, "after adding 11..15")
-    testEqualityRingBufferSlice(t, rb, s)
+	appendNumbers(rb, s, 5, &val, &pos)
+	logRingBuffer(t, rb, s, "after adding 11..15")
+	testEqualityRingBufferSlice(t, rb, s)
 
-    appendNumbers(rb, s, 10, &val, &pos)
-    logRingBuffer(t, rb, s, "after adding 16..25")
-    testEqualityRingBufferSlice(t, rb, s)
+	appendNumbers(rb, s, 10, &val, &pos)
+	logRingBuffer(t, rb, s, "after adding 16..25")
+	testEqualityRingBufferSlice(t, rb, s)
 }
