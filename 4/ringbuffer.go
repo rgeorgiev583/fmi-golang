a8834d317c9693c6820be3f6b05cd634de04ef73
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-12-31 04:50:36 +0200

    added Item method for accessing an element of the RingBuffer; added InvalidIndexError for RingBuffer; Stringer interface now implemented by RingBuffer

--- 4/ringbuffer.go
+++ 4/ringbuffer.go
@@ -1,16 +1,35 @@
 package main
 
+import (
+    "bytes"
+    "fmt"
+)
+
 type RingBuffer struct {
     buffer []interface{}
     beginPos, endPos int
 }
 
-func (rb *RingBuffer) Slice() []interface{} {
-    if rb.beginPos <= rb.endPos {
-        return rb.buffer[rb.beginPos:rb.endPos+1]
-    } else {
-        return append(rb.buffer[rb.beginPos:], rb.buffer[:rb.endPos+1])
+type InvalidIndexError struct {
+    index int
+}
+
+func (iie *InvalidIndexError) Error() string {
+    return fmt.Sprintf("%d is not a valid index for a ring buffer element", iie.index)
+}
+
+func (rb *RingBuffer) Item(index int) (interface{}, error) {
+    if index < 0 || index >= len(rb.buffer) {
+        return nil, &InvalidIndexError{index}
     }
+
+    pos := rb.beginPos + index
+
+    if pos >= len(rb.buffer) {
+        pos -= len(rb.buffer)
+    }
+
+    return rb.buffer[pos], nil
 }
 
 func (rb *RingBuffer) Append(item interface{}) {
@@ -34,6 +53,20 @@ func (rb *RingBuffer) Append(item interface{}) {
     }
 
     rb.buffer[rb.endPos] = item
+}
+
+func (rb *RingBuffer) String() string {
+    var buf bytes.Buffer
+    buf.WriteString("[ ")
+
+    for i := 0; i < len(rb.buffer) - 1; i++ {
+        rbi, _ := rb.Item(i)
+        buf.WriteString(fmt.Sprintf("%v ", rbi))
+    }
+
+    rbi, _ := rb.Item(len(rb.buffer) - 1)
+    buf.WriteString(fmt.Sprintf("%v]", rbi))
+    return buf.String()
 }
 
 func NewRingBuffer(size int) *RingBuffer {
